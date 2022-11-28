ceaf6e57482eef07ace4bf5a7c0e23400af48150
ABoyd <phoenix239@users.noreply.github.com>
2022-11-28 02:29:18 -0800

    Added Scroll to Top

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,8 @@
 // import ReactDOM from 'react-dom/client';
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
 
 import NotFoundPage from './pages/NotFoundPage';
-import PageTemplate from './pages/PageTemplate';
 import HomePage from './pages/HomePage';
 import About from './pages/About';
 import Contact from './pages/Contact';
@@ -13,46 +12,47 @@ import ShopCategory from './pages/ShopCategory';
 import ShopListing from './pages/ShopListing';
 import Shop from './pages/Shop';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
+  ScrollToTop();
   return (
-    <BrowserRouter>
-      <PageTemplate>
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='about' element={<About />} />
-          <Route path='contact' element={<Contact />} />
-          <Route path='gallery' element={<Gallery />} />
-          <Route
-            path='gallery/rings'
-            element={<GalleryImages title='Rings' />}
-          />
-          <Route
-            path='gallery/earrings'
-            element={<GalleryImages title='Earrings' />}
-          />
-          <Route
-            path='gallery/pendants'
-            element={<GalleryImages title='Pendants' />}
-          />
-          <Route
-            path='gallery/other'
-            element={<GalleryImages title='Other' />}
-          />
-          <Route
-            path='gallery/fabrication'
-            element={<GalleryImages title='Fabrication' />}
-          />
-          <Route path='shop' element={<Shop />} />
-          <Route path='shop/earrings' element={<ShopCategory />} />
-          <Route path='shop/rings' element={<ShopCategory />} />
-          <Route path='shop/pendants' element={<ShopCategory />} />
-          <Route path='shop/sets' element={<ShopCategory />} />
-          <Route path='shop/nose' element={<ShopCategory />} />
-          <Route path='shop/:category/:listing' element={<ShopListing />} />
-          <Route path='*' element={<NotFoundPage />} />
-        </Routes>
-      </PageTemplate>
-    </BrowserRouter>
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+      <Route path='gallery' element={<Gallery />} />
+      <Route path='gallery/rings' element={<GalleryImages title='Rings' />} />
+      <Route
+        path='gallery/earrings'
+        element={<GalleryImages title='Earrings' />}
+      />
+      <Route
+        path='gallery/pendants'
+        element={<GalleryImages title='Pendants' />}
+      />
+      <Route path='gallery/other' element={<GalleryImages title='Other' />} />
+      <Route
+        path='gallery/fabrication'
+        element={<GalleryImages title='Fabrication' />}
+      />
+      <Route path='shop' element={<Shop />} />
+      <Route path='shop/earrings' element={<ShopCategory />} />
+      <Route path='shop/rings' element={<ShopCategory />} />
+      <Route path='shop/pendants' element={<ShopCategory />} />
+      <Route path='shop/sets' element={<ShopCategory />} />
+      <Route path='shop/nose' element={<ShopCategory />} />
+      <Route path='shop/:category/:listing' element={<ShopListing />} />
+      <Route path='*' element={<NotFoundPage />} />
+    </Routes>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/App';
+
+import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+import App from './components/App';
+import PageTemplate from '../src/components/pages/PageTemplate';
 import theme from './components/theme';
+import './index.css';
 
 document.body.style.overflowY = 'scroll';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ThemeProvider theme={theme}>
-    <App />
+    <BrowserRouter>
+      <PageTemplate>
+        <App />
+      </PageTemplate>
+    </BrowserRouter>
   </ThemeProvider>
 );

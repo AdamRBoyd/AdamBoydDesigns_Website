44274f527c292c02a9926329c184a23539d866a2
ABoyd <phoenix239@users.noreply.github.com>
2023-02-12 17:40:17 -0800

    Changing the 404 page around

--- src/components/pages/NotFoundPage.jsx
+++ src/components/pages/NotFoundPage.jsx
@@ -1,34 +1,80 @@
 import React from 'react';
 import styled from 'styled-components';
-import { palette } from 'styled-theme';
+import { font, palette } from 'styled-theme';
 
-import { Heading, PageTitleFrame, Paragraph, Spacer } from '../../components';
+import {
+  Link,
+  Heading,
+  PageTitleFrame,
+  Paragraph,
+  Spacer,
+} from '../../components';
 
 const StyledHeading = styled(Heading)`
-  text-align: center;
   color: ${palette('primary', 0)};
-  font-size: 2.5rem;
+  text-align: center;
+  font-size: 2rem;
   text-transform: uppercase;
 `;
 
+const SubHeading = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: ${palette('grayscale', 5, true)};
+  font-family: ${font('primary')};
+  font-size: 1rem;
+  font-style: italic;
+`;
+
+const BodyText = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: ${palette('grayscale', 5, true)};
+  font-family: ${font('primary')};
+  font-size: 0.8rem;
+`;
+
+const StyledLink = styled(Link)``;
+
 const StyledParagraph = styled(Paragraph)`
-  text-align: center;
-  color: ${palette('grayscale', 0)};
+  font-weight: 600;
+  padding-bottom: 0.7rem;
+`;
+
+const ItalicFont = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: ${palette('grayscale', 5, true)};
+  font-family: ${font('primary')};
+  font-size: 0.8rem;
+  font-style: italic;
 `;
 
 const NotFoundPage = () => {
+  const currentPage = window.location;
+
   return (
-    <PageTitleFrame title='OOPS!' noBottomRule>
+    <PageTitleFrame title='404 Page Not Found' noBottomRule>
       <Spacer padding={0.5} />
-      <StyledHeading>404 Page Not Found</StyledHeading>
-      <StyledParagraph>
-        Don't know how you got here, but this is it... Not much to look at
-        huh...?
-      </StyledParagraph>
-      <StyledParagraph>
-        If you think you've found a bug, please let me know.
-      </StyledParagraph>
-      <StyledParagraph>Maybe try one of the links above...</StyledParagraph>
+      <StyledHeading>
+        Don't know how you got here, but this is it...
+      </StyledHeading>
+      <Spacer padding={1} />
+      <SubHeading>Not much to look at huh...?</SubHeading>
+      <Spacer padding={4} />
+      <BodyText>Ok, Let me take a look...</BodyText>
+      <Spacer padding={0.5} />
+      <BodyText>Ahhh, there's your problem...</BodyText>
+      <StyledParagraph>{`${currentPage} doesn't exist...`}</StyledParagraph>
+      <BodyText>Where were you trying to go? Cause this ain't it...</BodyText>
+      <Spacer padding={3} />
+      <ItalicFont>Maybe try one of the links above...</ItalicFont>
+      <Spacer padding={6} />
+      <StyledParagraph>If you think you've found a bug, </StyledParagraph>
+      <StyledLink to='/contact'> please let me know.</StyledLink>
     </PageTitleFrame>
   );
 };
